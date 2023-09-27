de8296dc17cc5b78284d5675a8f303dc062d3d9f
danielbrownmsm <daniel.brown@msmrockets.org>
2023-09-27 18:26:22 -0500

    added comments to annoy Dylan

--- autonav_ws/src/autonav_vision/src/camera_node.py
+++ autonav_ws/src/autonav_vision/src/camera_node.py
@@ -8,23 +8,31 @@ from cv_bridge import CvBridge
 
 from sensor_msgs.msg import CompressedImage
 
+# Bridge for converting ROS image stuff to OpenCV image stuff and back
 bridge = CvBridge()
 
 
+# A node to publish CompressedImage messages to the /camera/compressed topic for use by the computer vision node
 class CameraNode(Node):
     def __init__(self):
         super().__init__("camera_node")
 
+        # publisher for the /camera/compressed topic
         self.camera_publisher = self.create_publisher(CompressedImage, "/camera/compressed", 1)
 
+        # start publishing images
         self.runCamera()
 
     def runCamera(self):
+        # initialize the camera
         capture = cv2.VideoCapture(0)
-        
+
+        # just run until we're killed        
         while 1:
+            # no idea what ret is, but frame is the OpenCV image read from the camera stream
             ret, frame = capture.read()
 
+            # publish it as a CompressedImage to the topic
             self.camera_publisher.publish(bridge.cv2_to_compressed_imgmsg(frame))
     
 def main():
--- autonav_ws/src/autonav_vision/src/transformation_node.py
+++ autonav_ws/src/autonav_vision/src/transformation_node.py
@@ -8,21 +8,26 @@ from cv_bridge import CvBridge
 
 from sensor_msgs.msg import CompressedImage
 
+# Bridge for converting ROS image stuff to OpenCV image stuff and back
 bridge = CvBridge()
 
 
+# A node to handle all the thresholding and computer vision tasks for the robot
 class TransformationNode(Node):
     def __init__(self):
         super().__init__("vision_transformation_node")
 
+        # subscriber for the CompressedImage topic
         self.camera_subscriber = self.create_subscription(CompressedImage, "/camera/compressed", self.onImage, 10)
 
+        # publisher for the binary output image
         self.filtered_publisher = self.create_publisher(CompressedImage, "/autonav/camera/filtered", 1)
 
         # Threshold HSV values
         self.lower = (0, 0, 0) #TODO
         self.upper = (255, 255, 255) #TODO
     
+    # HSV thresholding for detecting obstacles/lines
     def thresholdImage(self, image):
         # apply the HSV threshold
         mask = cv.inRange(image, self.lower, self.upper)
@@ -32,7 +37,7 @@ class TransformationNode(Node):
         mask = 255 - mask
         mask[mask < 250] = 0
 
-
+    # /camera/compressed subscriber callback
     def onImage(self, data):
         # turn image into something we can work with
         image = bridge.compressed_imgmsg_to_cv2(data)
