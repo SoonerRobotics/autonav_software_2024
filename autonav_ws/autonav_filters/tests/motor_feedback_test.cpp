c9f67bb4a96a2c359bed97484c9ed9d0e5918bc7
antonioc76 <antoniochappell@protonmail.com>
2023-10-06 18:31:13 -0500

    so begins the unit testing adventure

---
+++ autonav_ws/autonav_filters/tests/motor_feedback_test.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "autonav_filters/particle_filter.hpp"
+
+TEST(ParticleFilterTests, MotorFeedbackTest) {
+    EXPECT_TRUE(false);
+}
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
