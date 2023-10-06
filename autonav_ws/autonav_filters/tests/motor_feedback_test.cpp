8248903c77922874f256904066e0a45eced01a50
antonioc76 <antoniochappell@protonmail.com>
2023-10-06 18:48:30 -0500

    i included a certain kind of header file

--- autonav_ws/autonav_filters/tests/motor_feedback_test.cpp
+++ autonav_ws/autonav_filters/tests/motor_feedback_test.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
+#include "autonav_filters/particle_filter.hpp"
 
 TEST(ParticleFilterTests, MotorFeedbackTest) {
+    float latitudeLength = 111086.2;
+    float longitudeLength = 81978.2;
+    ParticleFilter particle_filter{latitudeLength, longitudeLength};
     EXPECT_TRUE(false);
 }
 
