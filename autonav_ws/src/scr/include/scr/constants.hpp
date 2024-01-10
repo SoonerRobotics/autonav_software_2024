e6bb1ac30006960985deb53b392e425dfcfa640d
Dylan Zemlin <dylan.zemlin@gmail.com>
2024-01-10 15:49:42 -0600

    Added additional topics to constants

--- autonav_ws/src/scr/include/scr/constants.hpp
+++ autonav_ws/src/scr/include/scr/constants.hpp
@@ -20,4 +20,21 @@ namespace SCR
             const std::string CONFIG_UPDATE = "/scr/update_config_client";
         }
     }
+
+    namespace DZ
+    {
+        namespace Topics
+        {
+            const std::string POSITION = "/autonav/position";
+            const std::string MOTOR_FEEDBACK = "/autonav/MotorFeedback";
+            const std::string MOTOR_INPUT = "/autonav/MotorInput";
+            const std::string GPS_FEEDBACK = "/autonav/GPSFeedback";
+            const std::string IMU_DATA = "/autonav/imu";
+            const std::string CONBUS_DATA = "/autonav/conbus/data";
+            const std::string CONBUS_CONTROL = "/autonav/conbus/instruction";
+            const std::string CAMERA_RAW = "/autonav/camera/compressed";
+            const std::string CAMERA_FILTERED = "/autonav/cfg_space/raw/image";
+            const std::string CAMERA_ASTAR_DEBUG = "/autonav/debug/astar/image";
+        }
+    }
 }
