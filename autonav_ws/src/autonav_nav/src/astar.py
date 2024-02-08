ac52a3ed5d7574ee29905906deaac2b2978439a9
danielbrownmsm <daniel.brown@msmrockets.org>
2024-02-07 18:50:13 -0600

    copied and pasted someone else's A* code, will try to figure it out later

--- autonav_ws/src/autonav_nav/src/astar.py
+++ autonav_ws/src/autonav_nav/src/astar.py
@@ -27,8 +27,7 @@ CV_BRIDGE = cv_bridge.CvBridge()
 # there's gotta be a better way to store these, right? like, idk, csv file?
 competition_waypoints = [
     # Start Nomans, Frist Ramp, Middle Ramp, End Ramp, End Nomans
-    [(42.6682837222, -83.2193403028), (42.6681206444, -83.2193606083), (42.66809863885, -83.2193606083), (42.6680766333, -83.2193606083),
-     (42.6679277056, -83.2193276417), (42.6679817056, -83.2193316417), (42.66794, -83.2189), (42.6681268, -83.218887)],  # Start Facing North
+    [(42.6682837222, -83.2193403028), (42.6681206444, -83.2193606083), (42.66809863885, -83.2193606083), (42.6680766333, -83.2193606083), (42.6679277056, -83.2193276417), (42.6679817056, -83.2193316417), (42.66794, -83.2189), (42.6681268, -83.218887)],  # Start Facing North
     # [(42.6679277056,-83.2193276417),(42.6680766333,-83.2193591583),(42.6681206444,-83.2193606083),(42.6682837222 ,-83.2193403028), (42.668290020, -83.2189080), (42.6681268, -83.218887)], # Start Facing South
     # [(42.668222,-83.218472),(42.6680859611,-83.2184456444),(42.6679600583,-83.2184326556)], # Start Facing North
     # [(42.6679600583,-83.2184326556), (42.6680859611,-83.2184456444),(42.668222,-83.218472)], # Start Facing South
@@ -52,14 +51,11 @@ practice_waypoints = [
 simulation_waypoints = [
     [(35.19474407276026, -97.43853155838949), (35.19474444006683, -97.43853155838949), (35.19476343444084, -97.43853155838949), (35.194784303857695, -97.43853155838949), (35.19480517366998, -97.43853155838949), (35.194826043658466, -97.43853155838949), (35.194846913681346, -97.43853155838949), (35.19486778361504, -97.43853155838949), (35.19488865357613, -97.43853155838949), (35.19490952352433, -97.43853155838949), (35.19493039351497, -97.43853155908063), (35.19495092553071, -97.43853552083075), (35.19496904627728, -97.43854785157549), (35.19498133489532, -97.43856818587176), (35.194986852840486, -97.43859248699697), (35.19498544142225, -97.43861781734869), (35.19498216591245, -97.43864288876748), (35.194975713591994, -97.43866695750627), (35.19496281070465, -97.43868684693129), (35.19495206700054, -97.43870821496708), (35.19495306210649, -97.43873340525361), (35.19495373669383, -97.43875811465132), (35.1949525483157, -97.43876883071538), (35.19495038594336, -97.43877933293588), (35.1949494009205, -97.43878249462878), (35.19494565765572, -97.43878891692025), (35.19493780222935, -97.43879721568605), (35.19493412569523, -97.43880887472054), (35.194938969788005, -97.43881850730428),
      (35.19494614845655, -97.43882251503061), (35.19495732373083, -97.43884312856127), (35.194963623223224, -97.43886733394329), (35.19496923716838, -97.43889181952832), (35.194969734085255, -97.43891718333695), (35.19496864285037, -97.43894257267571), (35.19496499368157, -97.43896743978841), (35.19495396416087, -97.4389889380561), (35.19494005654008, -97.43900789250587), (35.1949230614983, -97.4390214352402), (35.19490221187155, -97.43902250398679), (35.194881438055944, -97.4390244107245), (35.19486225311435, -97.43903435637161), (35.19484266931345, -97.43904271965387), (35.19482218904126, -97.43903970952904), (35.194802405220315, -97.4390316150356), (35.19478237714437, -97.43902471055158), (35.19476156857851, -97.43902505092527), (35.194740700769096, -97.43902542041042), (35.19471983307143, -97.43902577288944), (35.194700320969666, -97.43903368010614), (35.19468202116007, -97.43904577057661), (35.19466155409562, -97.43904408814375), (35.19464101652687, -97.43903961268632), (35.194620588058406, -97.43903441002884), (35.19460015971028, -97.43902920615665), (35.194579731929494, -97.43902400228444), (35.19456317970144, -97.43901978556296), (35.194562940045294, -97.43901972465927)],
-    [(35.194888446528864, -97.43902288310593), (35.194765121094015, -97.43902321108108),
-     (35.19472527123724, -97.4390233172227), (35.19459435968777, -97.43902366555493)],
-    [(35.19474411, -97.43852997), (35.19474792, -97.43863678),
-     (35.19484711, -97.43865967), (35.19494247, -97.43875885)],
+    [(35.194888446528864, -97.43902288310593), (35.194765121094015, -97.43902321108108), (35.19472527123724, -97.4390233172227), (35.19459435968777, -97.43902366555493)],
+    [(35.19474411, -97.43852997), (35.19474792, -97.43863678), (35.19484711, -97.43865967), (35.19494247, -97.43875885)],
     # [(35.194816101646275, -97.43853155838949), (35.19496046401714, -97.43853155838949), (35.19501943703302, -97.4386386771562)],
     # [(35.19496918, -97.43855286), (35.19475331, -97.43860863), (35.19493775, -97.43881094)],
-    [(35.19496918, -97.43855286), (35.19498780, -97.43859524), (35.19495163, -97.43871339), (35.19495182, -97.43877829), (35.19493406, - \
-                                                                                                                          97.43879432), (35.19493812, -97.43881810), (35.19495368, -97.43882411), (35.19493357, -97.43902773), (35.19485847, -97.43902381)],
+    [(35.19496918, -97.43855286), (35.19498780, -97.43859524), (35.19495163, -97.43871339), (35.19495182, -97.43877829), (35.19493406, -97.43879432), (35.19493812, -97.43881810), (35.19495368, -97.43882411), (35.19493357, -97.43902773), (35.19485847, -97.43902381)],
 ]
 
 
@@ -92,6 +88,113 @@ def toSafetyLights(autonomous: bool, eco: bool, mode: int, brightness: int, colo
 #         self.waypointDirection = 0
 #         self.useOnlyWaypoints = False
 #         self.waypointDelay = 17.5
+
+
+# reference: https://llego.dev/posts/implementing-the-a-search-algorithm-python/ and https://en.wikipedia.org/wiki/A*_search_algorithm and https://stackabuse.com/courses/graphs-in-python-theory-and-implementation/lessons/a-star-search-algorithm/
+# class to represent a node in a graph for doing A* things
+class GraphNode:
+    # pos is an (x, y) coordinate pair, g_cost is the cost to move from the start to here, and h_cost is the estimated cost to move from here to the goal
+    def __init__(self, pos: tuple, g_cost: float, h_cost: float):
+        self.pos = pos
+        self.x = pos[0] # literally just for convienence
+        self.y = pos[1]
+
+        self.g_cost = g_cost # cost from start to here
+        self.h_cost = h_cost # cost from here to goal (estimated)
+        self.f_cost = g_cost + h_cost # total cost (man this is getting expensive)
+
+        self.parent = None # previous node
+    
+    # compare this node to another
+    def __lt__(self, other: GraphNode):
+        # if our total cost is less than we are less (obviously)
+        return self.f_cost < other.f_cost
+    
+class AStar:
+    def __init__(self, map_grid):
+        self.frontier = [] # list of open nodes we haven't seen
+        self.closed = [] # nodes we have visited 
+        self.map_grid = map_grid
+    
+    def search(self, start_node: GraphNode, goal_node: GraphNode):
+        self.frontier.append(start_node)
+
+        # while there are still nodes in the frontier
+        while self.frontier:
+            # sort the list to get the node with the lowest cost first
+            self.frontier.sort()
+            current_node = self.frontier.pop(0)
+
+            # add the current node to the list of closed nodes
+            self.closed.append(current_node)
+
+            # if our current node is the goal node, then we've reached it
+            if current_node == goal_node:
+                # reached the goal node
+                pass #TODO
+        
+            # check all the neighbors of the node
+            neighbors = self.get_neighbors(current_node)
+
+            # for each neighboring node
+            for neighbor in neighbors:
+                # if we've already searched it
+                if neighbor in self.closed:
+                    # move on
+                    continue
+                
+                # else, calculate its cost
+                g_cost = current_node.g_cost + 1 # cost to move
+                h_cost = self.heuristic(neighbor, goal_node) # cost to goal node
+                f_cost = g_cost + h_cost
+
+                # check if we've found a cheaper path
+                #TODO figure out what this code does
+                if neighbor in self.frontier:
+                    if neighbor.f_cost > f_cost:
+                        self.update_node(neighbor, g_cost, h_cost, current_node)
+                else:
+                    self.update_node(neighbor, g_cost, h_cost, current_node)
+        
+        return None # if we haven't returned anything by now, there isn't a path then
+
+    def get_neighbors(self, node: GraphNode) -> list:
+        neighbors = []
+
+        # neighbors are just the surrounding nodes
+        for x in (-1, 1, 1):
+            for y in (-1, 1, 1):
+                neighbor_pos = (node.x + x, node.y + y)
+
+                # check if it's in bounds
+                if (0 <= neighbor_pos.x <= self.map_grid.shape[0] and 0 <= neighbor_pos.y <= self.map_grid.shape[1]):
+                    # check if it's not an obstacle
+                    if self.map_grid[neighbor_pos] != 1:
+                        neighbors.append(neighbor_pos)
+
+        return neighbors
+
+    # manhattan distance from here to goal
+    def heuristic(self, node, goal):
+        return abs(node.x - goal.x) + abs(node.y - goal.y)
+    
+    # reconstruct the path 
+    def reconstruct_path(self, goal_node):
+        path = [goal_node]
+        current = goal_node
+
+        while current.parent != self.start_node:
+            path.append(current.parent)
+            current = current.parent
+
+        return path[::-1] #reverse path (so it's from start to finish instead of finish to start)
+    
+    def update_node(self, node, g_cost, h_cost, current_node):
+        node.g_cost = g_cost
+        node.h_cost = h_cost
+        node.f_cost = g_cost + h_cost
+        node.parent = current_node
+
 
 
 class AStarNode(Node):
