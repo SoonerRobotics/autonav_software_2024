b95ad40de1cf71f472b11b4186466f6ebcac1833
danielbrownmsm <daniel.brown@msmrockets.org>
2024-04-30 20:27:04 -0500

    minor bug fixes

--- autonav_ws/src/autonav_nav/src/astar.cpp
+++ autonav_ws/src/autonav_nav/src/astar.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 // C++ includes
 #include <algorithm>
 #include <filesystem>
@@ -184,7 +182,7 @@ public:
                 // and the y coord is in bounds
                 if (0 < y && y < MAX_Y) {
                     // and it's not an obstacle
-                    if (map[y][x] != 0) {
+                    if (map[MAX_X * y  +  x] != 0) { // row major order or something
                         // then it's a good neighbor
                         GraphNode neighbor;
                         neighbor.x = x;
@@ -204,7 +202,8 @@ public:
 
     std::vector<GraphNode> findPath() {
         // create a new, empty frontier
-        this->frontier = std::priority_queue<GraphNode>();
+        this->frontier = std::vector<GraphNode>(); //FIXME this needs to be a priorityqueue or something at some point yada yada linked list yada yada how many times has the type of this changed?
+        this->frontier.resize((MAX_X * MAX_Y) / 2); // reserve our initial space
 
         // make the start node
         GraphNode startNode;
@@ -215,7 +214,7 @@ public:
         startNode.f_cost = INF_COST;
 
         // push the starting node for our search onto the frontier
-        this->frontier.push(startNode);
+        this->frontier.push_back(startNode);
 
         // combined smellification algorithm
         int depth = 0;
@@ -231,24 +230,26 @@ public:
         while (depth < MAX_DEPTH) {
             // for each node on our frontier
             while(frontier.size() > 0) {
-                node = frontier.top(); // the node we look at first is the one with the least cost
-                frontier.pop(); // remove it from the frontier
+                node = frontier[0]; // the node we look at first is the one with the least cost
+                frontier.erase(frontier.begin()); // remove it from the frontier (https://stackoverflow.com/questions/875103/how-do-i-erase-an-element-from-stdvector-by-index)
 
                 // for each neighbor of the node
                 for (GraphNode neighbor : getNeighbors(node)) {
                     // if the neighbor is already in the to-be-explored list
-                    if (neighbor in frontier || neighbor in closed) {
+                    //FIXME maybe this also needs to check if it's already in the closed list???
+                    if (std::find(closed.begin(), closed.end(), neighbor) != closed.end()) { //https://stackoverflow.com/questions/571394/how-to-find-out-if-an-item-is-present-in-a-stdvector
                         continue; // skip it
                     }
 
                     // calculate smellification costs
                     double x_cost = pow((neighbor.x - (MAX_X)/2), 2); // cost for x coordinate is (x - MAX/2)**2
-                    double y_cost = -neighbor.y + MAX_Y; // cost for y coordinate is just linear
+                    double y_cost = -neighbor.y + MAX_Y; // cost for y coordinate is just linear (keep in mind that the top of the grid is y=0 and we are at y=MAX_Y)
 
                     // update A* costs
                     double g_cost = node.g_cost + 1; // cost to move there is current+1
                     //TODO should this be x_cost and y_cost?
                     double h_cost = dist(neighbor, startNode); // h_cost is just distance from here to the goal except we don't know where the goal is we're figuring that out as we go FIXME
+                    h_cost = x_cost + y_cost; //FIXME
 
                     // otherwise, update it
                     neighbor.parent = &node; // neighbor should point to current node for it's parent
@@ -257,7 +258,7 @@ public:
                     neighbor.f_cost = g_cost + h_cost;
 
                     // and add it to the frontier
-                    this->frontier.push(neighbor);
+                    this->frontier.push_back(neighbor);
                 }
 
                 // if the current node is cheaper to go to
@@ -293,8 +294,12 @@ public:
             // and move our current back to the node
             current = node;
         }
+
+        // sort the frontier so lowest-cost is first and gets searched first
+        std::sort(frontier.begin(), frontier.end()); //https://stackoverflow.com/questions/2758080/how-to-sort-an-stl-vector
     }
 
+    // return the distance between two graphNodes
     double dist(GraphNode here, GraphNode there) {
         return sqrt((here.x - there.x)*(here.x-there.x)  +  (here.y-there.y)*(here.y-there.y));
     }
@@ -306,10 +311,9 @@ private:
 
     // member fields
     //https://www.geeksforgeeks.org/priority-queue-in-cpp-stl/ and  https://cplusplus.com/reference/queue/priority_queue/
-    std::priority_queue<GraphNode> frontier; // priority queue (aka heap, heapqueue, etc) of all the points we need to explore next for A* (priority queue is used because it is fast and good)
+    std::vector<GraphNode> frontier; // list of all the points we need to explore next for A* (priority queue would be used because it is fast and good but we didn't)
     std::vector<GraphNode> closed; // nodes we have explored
-    // std::vector<std::vector<int>> map; // local map data
-    std::vector<signed char, std::allocator<signed char>> map; //FIXME ?????
+    std::vector<int> map; // to hold the map data from the OccupancyGrid msg
     GPSPoint position; // position of robot (lat, lon)
     std::vector<GPSPoint> waypoints; // gps waypoints we're PIDing to
 
