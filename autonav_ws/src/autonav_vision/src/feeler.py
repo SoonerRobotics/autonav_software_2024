8fd2c6db534a28e955192e3a8f1cd0f3b9b2821a
danielbrownmsm <daniel.brown@msmrockets.org>
2024-06-29 09:42:09 -0500

    combine the separate things I've been working on into one big thing mostly using the vector stuff so we can do easy math also on a road trip but this time to North Carolina instead of IGVC

--- autonav_ws/src/autonav_vision/src/feeler.py
+++ autonav_ws/src/autonav_vision/src/feeler.py
@@ -3,21 +3,6 @@ from math import cos, sin, atan, radians, degrees
 import tkinter
 from tkinter import filedialog
 
-
-class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def __add__(self, other):
-        return self.x + other.x, self.y + other.y
-    
-    def __sub__(self, other):
-        return self.x - other.x, self.y - other.y
-    
-    def __neg__(self):
-        return -self.x, -self.y
-
 MAX_LENGTH = 100
 
 # colors
@@ -26,47 +11,72 @@ WHITE = (255, 255, 255)
 WIDTH = 800
 HEIGHT = 800
 
-# standard distance formula
-def dist(x, y):
-    return (self.x ** 2  +  self.y ** 2) ** 0.5
-
+#FIXME do we want this to be a class method of vector or feeler or whatever?
 # convert x and y coordinates so that they are relative to the center of the image
 def centerCoordinates(x, y):
     return (x + WIDTH//2), (y + HEIGHT//2)
 
-# convert polar coordiantes of the feelers (distance and angle) to x and y coordinates
-def polarToXY(distance, angle):
-    x = distance * cos(radians(angle))
-    y = distance * sin(radians(angle))
+class Vector:
+    # so there's no confusion when creating a vector because we are mixing coordinate systems all over the place,
+    # just make a 0 vector and then you're supposed to call either setXY or setPolar to actually change the values
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.angle = 0
+        self.legnth = 0
 
-    return x, y
-
-#TODO is this supposed to work with centered- or left-upper-corner- origin coordinates?
-def xyToPolar(x, y):
-    length = dist(x, y)
-    angle = degrees(atan(y / x))
-
-
-class Feeler:
-    def __init__(self, max_length, angle):
-        self.max_length = length
+    # def __init__(self, x, y):
+    #     self.x = x
+    #     self.y = y
+    #     self.updatePolar()
+    
+    # def __init__(self, angle, length):
+    #     self.angle = angle
+    #     self.lenght = length
+    #     self.updateCartesian()
+    
+    def getXY(self):
+        return self.x, self.y
+    
+    def getPolar(self):
+        return self.angle, self.length
+    
+    def setXY(self, x, y):
+        self.x = x
+        self.y = y
+        self.updatePolar()
+    
+    def setPolar(self, angle, length):
         self.angle = angle
-        self.length = max_length
-        
-    def update(self, image):
-        length = MAX_LENGTH
-
-        #TODO figure out how to pixel walk along the line or something
-        #TODO need to do lerp (linear interpolation, which is like how they draw lines or something)
-        for pixel in pixels:
-            if pixel >= OFF_WHITE:
-                x, y = pixel - (0, 0)
-
-                self.length, self.angle = xyToPolar(x, y)
+        self.length = length
+        self.updateCartesian()
+    
+    def setLength(self, length):
+        self.length = length
+        self.updateCartesian()
+    
+    def __add__(self, other):
+        return self.x + other.x, self.y + other.y
+    
+    def __sub__(self, other):
+        return self.x + other.x, self.y + other.y
+    
+    # called when polar coords have been set and need to update the associated cartesian ones
+    def updateCartesian(self):
+        #SOH CAH TOA
+        #sin(theta) = x / length
+        self.x = self.length * cos(self.angle)
+        self.y = self.length * sin(self.angle)
+    
+    # called for when cartesian coords are updated but need to update the polar ones
+    def updatePolar(self):
+        # good 'ol distance formula (assuming (0,0) is the origin for both polar and cartesian)
+        self.length = sqrt(self.x**2 + self.y**2)
+        self.angle = degrees(atan(self.y / self.x)) #TODO verify if this is correct
 
     # draw the feeler on the given image
     def draw(self, image):
-        cv2.line(image, centerCoordinates(0, 0), centerCoordinates(*polarToXY(self.length, self.angle)))
+        cv2.line(image, centerCoordinates(0, 0), centerCoordinates(self.x, self.y))
 
 class Robot:
     def __init__(self):
@@ -75,21 +85,28 @@ class Robot:
         self.vel = 0
         self.heading = 0
 
-        self.feelers = [Feeler(MAX_LENGTH, angle) for angle in range(0, 360, 10)]
+        self.feelers = []
+        for angle in range(0, 360, 10):
+            v = Vector()
+            v.setPolar(angle, MAX_LENGTH)
 
-        # start pionting straight
-        self.heading_arrow = Feeler(MAX_LENGTH, 0)
+            self.feelers.append(v)
+
+        # start pointing straight
+        self.heading_arrow = Vector(MAX_LENGTH, 0)
     
     def update(self):
         # reset our heading
-        self.heading_arrow.length = 0
-        self.heading_arrow.angle = 0
+        self.heading_arrow.setPolar(0, 0)
 
         for feeler in self.feelers:
-            # should be negative, so the addition works out right
-            delta = feeler.length - feeler.max_length
+            # make a vector from the end of the current vector if it was at max length to the end of the vector at its current length
+            error = Vector(MAX_LENGTH, feeler.angle) - feeler
 
-            #TODO add this vector to main heading arrow
+            # add this vector to main heading arrow
+            self.heading_arrow += error
+        
+        #TODO I think there's something else we need to do?
     
     def draw(self, image):
         pass #TODO
@@ -110,17 +127,16 @@ while video.isOpened():
     if not ret:
         break # the end of the video
     
-    #TODO do we want to read in the camera feed, the thresholded stuff, or the astar stuff?
-    # I'm thinking we do just the cameras, cause I don't think we want to use the same perspective transform parameters, or any at all
-    
+    mask = threshold(image)
+
     for feeler in robot.feelers:
-        feeler.update(image)
-        feeler.draw(image)
+        feeler.update(mask)
+        feeler.draw(image) #TODO do we want to output the mask or the image?
     
     robot.update()
     robot.draw(image)
 
     cv2.imshow("image", image)
-    cv2.waitKey(0)
+    cv2.waitKey(0) #TODO do we want to make this match the 8 fps or something? or videoWrite and not bother with real-time output?
 
 cv2.destroyAllWindows()
